a614746fff438609a5cfad5cf61a5f22ee4666c8
deep-1405 <dabhideep44@gmail.com>
2024-01-22 15:49:10 +0530

    Added flights search crud with few filters

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -24,9 +24,30 @@ class FlightService {
         }
     }
 
-    async getFlightData() {
-        
+    async getAllFlightData(data) {
+        try {
+            const flights = await this.flightrespository.getAllFlights(data);
+            return flights;
+        } catch (error) {
+            console.log("Something went wrong at service layer");
+            throw {error};
+        }
     }
+
+    
 }
 
 module.exports = FlightService;
+
+/**
+ * {
+ *   flightNumber,
+ *  airplaneId ,
+ *   departureAirportId,
+ *   arrivalAirportId,
+ *   arrivalTime,
+ *   departureTime,
+ *   price
+ *   totalSeats -> airplane
+ * }
+ */
